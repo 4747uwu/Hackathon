7915dca8ecbefa5dea4bbf17b206c62d3aae346b
shuklagabhishek <shuklagabhishek@gmail.com>
2025-02-03 23:26:16 +0530

    working again

---
+++ backend/models/noteSchema.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema(
+  {
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    mentions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    reactions: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        emoji: String, // Store reaction emojis
+      },
+    ],
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        text: String,
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Note = mongoose.model("Note", noteSchema);
+export default Note;
---
+++ backend/routes/noteRoute.js
@@ -0,0 +1,3 @@
+import express from 'express';
+import authMiddleware from '../middleware/authMiddleware.js';
+import Note from '../models/noteSchema.js';
